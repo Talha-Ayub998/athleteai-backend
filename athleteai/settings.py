54038d27c9d652a4628ec764cb78bd7b88acfa11
Talha-Ayub998 <talhaayub998@gmail.com>
2025-06-11 22:49:10 +0500

    For testing JWT Token

--- athleteai/settings.py
+++ athleteai/settings.py
@@ -62,8 +62,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=2),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 }
