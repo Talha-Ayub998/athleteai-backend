4971794c2ecc7b020b5232e381dad871c8f0105a
Talha-Ayub998 <talhaayub998@gmail.com>
2025-08-22 15:27:13 +0500

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,26 +1,60 @@
+# users/admin.py
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser, Subscription, ReportPurchase
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ('email', 'username', 'role', 'is_staff', 'is_active', 'is_superuser')
-    list_filter = ('role', 'is_staff', 'is_active')
-    search_fields = ('email', 'username')
-    ordering = ('email',)
 
+class SubscriptionInline(admin.StackedInline):
+    model = Subscription
+    can_delete = False
+    extra = 0
+    readonly_fields = ("stripe_customer_id", "stripe_subscription_id", "status", "current_period_end", "cancel_at_period_end")
+
+
+class ReportPurchaseInline(admin.TabularInline):
+    model = ReportPurchase
+    extra = 0
+    readonly_fields = ("stripe_payment_intent", "amount", "created_at")
+    fields = ("stripe_payment_intent", "amount", "created_at")
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+    # What shows in the user list
+    list_display = ("email", "username", "role", "is_active", "is_staff", "is_superuser", "date_joined", "last_login")
+    list_filter = ("role", "is_active", "is_staff", "is_superuser")
+    search_fields = ("email", "username")
+    ordering = ("email",)
+
+    # Use email as the identifier instead of username
     fieldsets = (
-        (None, {'fields': ('email', 'username', 'password', 'role')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Relations', {'fields': ('managed_users',)}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {"fields": ("email", "username", "password", "role")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Relations", {"fields": ("managed_users",)}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
-
     add_fieldsets = (
         (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'username', 'password1', 'password2', 'role', 'is_staff', 'is_active')}
-        ),
+            "classes": ("wide",),
+            "fields": ("email", "username", "password1", "password2", "role", "is_active", "is_staff"),
+        }),
     )
 
-admin.site.register(CustomUser, CustomUserAdmin)
+    # Attach inlines
+    inlines = [SubscriptionInline, ReportPurchaseInline]
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("user", "plan", "interval", "status", "current_period_end", "cancel_at_period_end")
+    list_filter = ("plan", "interval", "status", "cancel_at_period_end")
+    search_fields = ("user__email", "stripe_customer_id", "stripe_subscription_id")
+    readonly_fields = ("stripe_customer_id", "stripe_subscription_id", "current_period_end")
+
+
+@admin.register(ReportPurchase)
+class ReportPurchaseAdmin(admin.ModelAdmin):
+    list_display = ("user", "amount", "stripe_payment_intent", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("user__email", "stripe_payment_intent")
+    readonly_fields = ("stripe_payment_intent", "amount", "created_at")
